9ffa5eb2b6f12e6b2d02ceee87ca431c35278085
King <briomwaura@gmail.com>
2024-03-18 13:32:24 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Homepage';
+
 
 function App() {
   return (
     <div>
-     
+      < Home />
     </div>
   );
 }
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,7 +1,17 @@
-Home (){
+function Home (){
+
     return(
-        <>
-        </>
+        <div>
+            <nav>
+                <a href="/home">Home</a>
+                <a href="about">About</a>
+            </nav>
+            <div>
+                <section>
+                    It looks awesome
+                </section>
+            </div>
+        </div>
     )
 }
 export default Home
